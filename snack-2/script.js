1e98fedff0d92185e066171060c034b946b2908e
yuricifiello <yuricifiello@gmail.com>
2025-04-22 14:50:11 +0200

    snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,10 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
+
+people.forEach((person) => console.log(person.name));
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
