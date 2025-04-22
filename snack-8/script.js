7a7cd33e0adb99a428c0ad824860d2005ef9c20b
yuricifiello <yuricifiello@gmail.com>
2025-04-22 15:06:42 +0200

    snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,23 +1,28 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    class: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    class: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    class: "5B",
   },
 ];
+
+const student = students.find((student) => student.name === "Marco Lanci");
+const studentClass = student.class;
+
+console.log(studentClass);
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
